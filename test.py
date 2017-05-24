3a638a3974182c4bc994e46b70cc83a33d344e60
Ellery Queen <yhmsc007@hotmail.com>
2017-05-24 15:48:11 +0800

    2333

--- test.py
+++ test.py
@@ -4,4 +4,9 @@ a = tf.constant(5, name="input_a")
 b = tf.constant(3, name="input_b")
 c = tf.multiply(a, b, name="mul_c")
 d = tf.add(a, b, name="add_d")
-e = tf.add(a, b, name="add_e")
+e = tf.add(c, d, name="add_e")
+
+sees = tf.Session()
+print(sees.run(e))
+
+sees.graph
